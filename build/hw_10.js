d3214115f11da40c588be120ad455da26d61c2bf
Kryzhanivskyi_Oleksandr <oleksandr.kryzhanivskyi90@gmail.com>
2018-06-21 03:44:04 +0300

    Add files via upload
    release v2.0

---
+++ build/hw_10.js
@@ -0,0 +1,93 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, {
+/******/ 				configurable: false,
+/******/ 				enumerable: true,
+/******/ 				get: getter
+/******/ 			});
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "";
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = 330);
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ 330:
+/***/ (function(module, exports, __webpack_require__) {
+
+module.exports = __webpack_require__(331);
+
+
+/***/ }),
+
+/***/ 331:
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+
+
+__webpack_require__(332);
+
+/***/ }),
+
+/***/ 332:
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed: ModuleBuildError: Module build failed: \r\n\tjustify-content;\r\n^\r\n      Property \"justify-content\" must be followed by a ':'\r\n      in c:\\Users\\Alex\\Desktop\\web_academy_courses\\шаблонная папка\\src\\hw_10\\hw_10.scss (line 3, column 2)\n    at runLoaders (c:\\Users\\Alex\\Desktop\\web_academy_courses\\шаблонная папка\\node_modules\\webpack\\lib\\NormalModule.js:195:19)\n    at c:\\Users\\Alex\\Desktop\\web_academy_courses\\шаблонная папка\\node_modules\\loader-runner\\lib\\LoaderRunner.js:364:11\n    at c:\\Users\\Alex\\Desktop\\web_academy_courses\\шаблонная папка\\node_modules\\loader-runner\\lib\\LoaderRunner.js:230:18\n    at context.callback (c:\\Users\\Alex\\Desktop\\web_academy_courses\\шаблонная папка\\node_modules\\loader-runner\\lib\\LoaderRunner.js:111:13)\n    at Object.asyncSassJobQueue.push [as callback] (c:\\Users\\Alex\\Desktop\\web_academy_courses\\шаблонная папка\\node_modules\\sass-loader\\lib\\loader.js:55:13)\n    at Object.done [as callback] (c:\\Users\\Alex\\Desktop\\web_academy_courses\\шаблонная папка\\node_modules\\neo-async\\async.js:7974:18)\n    at options.error (c:\\Users\\Alex\\Desktop\\web_academy_courses\\шаблонная папка\\node_modules\\node-sass\\lib\\index.js:294:32)");
+
+/***/ })
+
+/******/ });
