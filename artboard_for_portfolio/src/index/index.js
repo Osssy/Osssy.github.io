2b1761e20c9271006a9189ac876aa4376b621bb4
Osssy <oleksandr.kryzhanivskyi90@gmail.com>
2019-09-29 15:22:03 +0300

    rebuild with .gitignore file

---
+++ artboard_for_portfolio/src/index/index.js
@@ -0,0 +1,8 @@
+// all styles
+import './index.scss';
+
+// custom js modules
+import './scripts/sideNavToggle.js';
+import './scripts/spoiler.js';
+import './scripts/slider.js';
+import './scripts/readMore.js';
